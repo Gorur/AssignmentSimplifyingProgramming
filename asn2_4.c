1eb68b5abeb49d86380bc3f9a0477fc792ce6802
Srilakshmi <gorursrilakshmi@gmail.com>
2014-09-15 16:48:01 +0530

    I tried re-doing the assignment 2

--- asn2_4.c
+++ asn2_4.c
@@ -1,536 +1,96 @@
 #include<stdio.h>
 
+#include<stdlib.h>
 
+#include<string.h>
 
-#define MAX 20
+#include<time.h>
 
+#include<unistd.h>
 
 
-int main() {
 
-	int i;
+#define MAX 30
 
-	char company_name1[MAX]="Google";
 
-	char company_name2[MAX]="Twitter";
+int main(int argc, char ** argv) {
 
-	char company_name3[MAX]="Facebook";
+	int c,i,val_i,j,k=0;
 
-	char company_name4[MAX]="Apple";
+	char str[MAX], *x[MAX];
 
-	char company_name5[MAX]="Microsoft";
+	char *fvalue=NULL, *lvalue=NULL,lFlag=0,ch;
 
-	char company_name6[MAX]="Hewlett-Packard";
+	char lines[MAX][BUFSIZ];
 
-	char company_name7[MAX]="Intel";
+	FILE *fp;
 
-	char company_name8[MAX]="Cisco";
+	while((c=getopt(argc,argv,"l:f:")) != -1) {
 
-	char company_name9[MAX]="Oracle";
-
-	char company_name10[MAX]="Amazon";
-
-
-	float share_value1,share_value2,share_value3,share_value4,share_value5,share_value6,share_value7,share_value8,share_value9,share_value10;
-
-	float share_value1_1,share_value2_1,share_value3_1,share_value4_1,share_value5_1,share_value6_1,share_value7_1,share_value8_1,share_value9_1,share_value10_1;
-
-	float share_value1_2,share_value2_2,share_value3_2,share_value4_2,share_value5_2,share_value6_2,share_value7_2,share_value8_2,share_value9_2,share_value10_2;
-
-	share_value1=575.70;
-
-	share_value2=51.93;
-
-	share_value3=77.48;
-
-	share_value4=101.66;
-
-	share_value5=46.70;
-
-	share_value6=36.56;
-
-	share_value7=34.58;
-
-	share_value8=25.16;
-
-	share_value9=40.5;
-
-	share_value10=331.19;
-
-
-
-	printf("The Share values of Companies as on Friday 12/09/2014 are as follows:\n");
-
-	printf("%s , $ %.2lf\n",company_name1,share_value1);
-
-	printf("%s , $ %.2lf\n",company_name2,share_value2);
-
-	printf("%s , $ %.2lf\n",company_name3,share_value3);
-
-	printf("%s , $ %.2lf\n",company_name4,share_value4);
-
-	printf("%s , $ %.2lf\n",company_name5,share_value5);
-
-	printf("%s , $ %.2lf\n",company_name6,share_value6);
-
-	printf("%s , $ %.2lf\n",company_name7,share_value7);
-
-	printf("%s , $ %.2lf\n",company_name8,share_value8);
-
-	printf("%s , $ %.2lf\n",company_name9,share_value9);
-
-	printf("%s , $ %.2lf\n",company_name10,share_value10);
-
-
-
-	printf("The Share values of Companies as on Monday 15/09/2014 are as follows:\n");
-
-	share_value1_1=569.85;
-
-	share_value2_1=51.40;
-
-	share_value3_1=77.04;
-
-	share_value4_1=101.89;
-
-	share_value5_1=46.39;
-
-	share_value6_1=36.36;
-
-	share_value7_1=34.53;
-
-	share_value8_1=25.13;
-
-	share_value9_1=40.32;
-
-	share_value10_1=331.85;
-
-
-	
-if(share_value1>share_value1_1) {
-
-		printf("%s,$ %.2lf,Down\n",company_name1,share_value1_1);
-
-	}
-
-	else if(share_value1<share_value1_1) {
-
-		printf("%s, $ %.2lf,Up\n",company_name1,share_value1_1);
-
-	}
-
-	else {
-
-		printf("%s, $ %.2lf,None\n",company_name1,share_value1);
-
-	}
-
-
-
-	if(share_value2>share_value2_1) {
-
-	        printf("%s,$ %.2lf,Down\n",company_name2,share_value2_1);
-
-	}
-       
-	else if(share_value2<share_value2_1) {
-
-	        printf("%s, $ %.2lf,Up\n",company_name2,share_value2_1);
-
-	}
-
-	else {
-
-		printf("%s, $ %.2lf,None\n",company_name2,share_value2);
-
-	}
-
-
-
-        if(share_value3>share_value3_1) {
-
-		printf("%s,$ %.2lf,Down\n",company_name3,share_value3_1);
-
-	}
-       
-	else if(share_value3<share_value3_1) {
-
-	        printf("%s, $ %.2lf,Up\n",company_name3,share_value3_1);
-
-	}
-
-	else {
-
-		printf("%s, $ %.2lf,None\n",company_name3,share_value3);
-
-	}
-
-
-
-        if(share_value4>share_value4_1) {
-
-		printf("%s,$ %.2lf,Down\n",company_name4,share_value4_1);
-
-        }
-
-        else if(share_value4<share_value4_1) {
-
-                printf("%s, $ %.2lf,Up\n",company_name4,share_value4_1);
-
-        }
-
-	else {
-
-		printf("%s, $ %.2lf,None\n",company_name4,share_value4);
-
-	}
-
-
-        
-	if(share_value5>share_value5_1) {
-
-		printf("%s,$ %.2lf,Down\n",company_name5,share_value5_1);
-
-	}      
-	
-	else if(share_value5<share_value5_1) {
-
-	        printf("%s, $ %.2lf,Up\n",company_name5,share_value5_1);
-
-	}
-
-	else {
-
-		printf("%s,$ %.2lf,None\n",company_name5,share_value5);
-
-	}
-
-
-
-        if(share_value6>share_value6_1) {
-
-		printf("%s,$ %.2lf,Down\n",company_name6,share_value6_1);
-
-	}
-       
-	else if(share_value6<share_value6_1) {
-
-	        printf("%s, $ %.2lf,Up\n",company_name6,share_value6_1);
-
-	}
-
-	else {
-
-		printf("%s, $ %.2lf,None\n",company_name6,share_value6);
-
-	}
-
-
-
-        if(share_value7>share_value7_1) {
-
-		printf("%s,$ %.2lf,Down\n",company_name7,share_value7_1);
-
-	}
-       
-	else if(share_value7,share_value7_1) {
-
-	        printf("%s, $ %.2lf,Up\n",company_name7,share_value7_1);
-
-	}
-
-	else {
-
-		printf("%s, $ %.2lf,None\n",company_name7,share_value7);
-
-	}
-
-
-
-        if(share_value8>share_value8_1) {
-
-		printf("%s,$ %.2lf,Down\n",company_name8,share_value8_1);
-
-	}
-       
-	else if(share_value8<share_value8_1) {
-
-	        printf("%s, $ %.2lf,Up\n",company_name8,share_value8_1);
-
-	}
-
-	else {
-
-		printf("%s, $ %.2lf,None\n",company_name8,share_value8);
-
-	}
-
-
-
-        if(share_value9>share_value9_1) {
-
-		printf("%s,$ %.2lf,Down\n",company_name9,share_value9_1);
-
-	}
-       
-	else if(share_value9<share_value9_1) {
-
-	        printf("%s, $ %.2lf,Up\n",company_name9,share_value9_1);
-
-	}
-
-	else {
-
-		printf("%s,$ %.2lf,None\n",company_name9,share_value9);
-
-	}
-
-
-
-        if(share_value10>share_value10_1) {
-
-                printf("%s,$ %.2lf,Down\n",company_name10,share_value10_1);
-
-        }
-
-        else if(share_value10<share_value10_1) {
-
-                printf("%s, $ %.2lf,Up\n",company_name10,share_value10_1);
-
-        }
-
-	else {
-
-		printf("%s, $ %.2lf,None\n",company_name10,share_value10);
-
-	}
-
-	
-
-	printf("The Share values of Companies as on Tuesday 16/09/2014 are as follows:\n");
-
-        share_value1_2=575.70;
-
-	share_value2_2=51.40;
-
-	share_value3_2=76.48;
-	
-	share_value4_2=101.89;
-
-	share_value5_2=46.39;
-
-	share_value6_2=37.36;
-
-	share_value7_2=34.53;
-
-	share_value8_2=25.13;
-
-	share_value9_2=39.69;
-
-	share_value10_2=332.85;
-
-
-
-        if(share_value1_1>share_value1_2) {
-
-		printf("%s,$ %.2lf,Down\n",company_name1,share_value1_2);
-
-	}
-
-        else if(share_value1_1<share_value1_2) {
-
-                printf("%s, $ %.2lf,Up\n",company_name1,share_value1_2);
-
-        }
-
-        else {
-
-                printf("%s, $ %.2lf,None\n",company_name1,share_value1_1);
-
-        }
-
-
-
-	if(share_value2_1>share_value2_2) {
-
-                printf("%s,$ %.2lf,Down\n",company_name2,share_value2_2);
-
-        }
-
-        else if(share_value2_1<share_value2_2) {
-
-                printf("%s, $ %.2lf,Up\n",company_name2,share_value2_2);
-
-        }
-
-        else {
-
-                printf("%s, $ %.2lf,None\n",company_name2,share_value2_1);
-
-        }
-
-
-
-	if(share_value3_1>share_value3_2) {
-
-                printf("%s,$ %.2lf,Down\n",company_name3,share_value3_2);
-
-        }
-
-        else if(share_value3_1<share_value3_2) {
-
-                printf("%s, $ %.2lf,Up\n",company_name3,share_value3_2);
-
-        }
-
-        else {
-
-                printf("%s, $ %.2lf,None\n",company_name3,share_value3_1);
-
-        }
-
-
-
-	if(share_value4_1>share_value4_2) {
-
-                printf("%s,$ %.2lf,Down\n",company_name4,share_value4_2);
-
-        }
-
-        else if(share_value4_1<share_value4_2) {
-
-                printf("%s, $ %.2lf,Up\n",company_name4,share_value4_2);
-
-        }
-
-        else {
-
-                printf("%s, $ %.2lf,None\n",company_name4,share_value4_1);
-
-        }
-
-
+		switch(c) {
  
-	if(share_value5_1>share_value5_2) {
+			case 'f':fvalue=optarg;
 
-                printf("%s,$ %.2lf,Down\n",company_name5,share_value5_2);
+				if(strcmp(fvalue,"share.csv") !=0) {
 
-        }
+					printf("Can't open file\nThe file was share.csv.TRY AGAIN!!!\n");
 
-        else if(share_value5_1<share_value5_2) {
+					exit(EXIT_FAILURE);
 
-                printf("%s, $ %.2lf,Up\n",company_name5,share_value5_2);
+				}
 
-        }
-
-        else {
-
-                printf("%s,$ %.2lf,None\n",company_name5,share_value5_1);
-
-        }
+				break;
 
 
+			case 'l':lFlag=1;
 
-	if(share_value6_1>share_value6_2) {
+				lvalue=optarg;
 
-                printf("%s,$ %.2lf,Down\n",company_name6,share_value6_2);
+				if(strcmp(lvalue,"log.txt") != 0) {
 
-        }
+					printf("Can't open the required file\n");
 
-        else if(share_value6_1<share_value6_2) {
+					printf("The file to be opened was log.txt. TRY AGAIN!!!\n");
+    
+					exit(EXIT_FAILURE);
 
-                printf("%s, $ %.2lf,Up\n",company_name6,share_value6_2);
+				}
 
-        }
+				break;
 
-        else {
+			default:
 
-                printf("%s, $ %.2lf,None\n",company_name6,share_value6_1);
+				printf("Invaild input \n");
 
-        }
+				exit(EXIT_FAILURE);
 
+			}
 
+	}
 
-	if(share_value7_1>share_value7_2) {
+	fp=fopen("share.csv","r");
 
-                printf("%s,$ %.2lf,Down\n",company_name7,share_value7_2);
+	if( fp == NULL ) {
 
-        }
+		perror("Error while opening the file.\n");
 
-        else if(share_value7_1<share_value7_2) {
+     		exit(EXIT_FAILURE);
 
-                printf("%s, $ %.2lf,Up\n",company_name7,share_value7_2);
-
-        }
-
-        else {
-
-                printf("%s, $ %.2lf,None\n",company_name7,share_value7_1);
-
-        }
-
-
+   	}
 	
-	if(share_value8_1>share_value8_2) {
+printf("Successfully file opened\n");
 
-	        printf("%s,$ %.2lf,Down\n",company_name8,share_value8_2);
-
-        }
-
-        else if(share_value8_1<share_value8_2) {
-
-                printf("%s, $ %.2lf,Up\n",company_name8,share_value8_2);
-
-        }
-
-        else {
-
-                printf("%s, $ %.2lf,None\n",company_name8,share_value8_1);
-
-        }
+	printf("The contents of file are\n");
 
 
- 
-	if(share_value9_1>share_value9_2) {
+        while((ch=fgetc(fp)) != EOF) {
 
-                printf("%s,$ %.2lf,Down\n",company_name9,share_value9_2);
+		printf("%c",ch);
 
-        }
+	}
 
-        else if(share_value9_1<share_value9_2) {
+	fclose(fp);
 
-                printf("%s, $ %.2lf,Up\n",company_name9,share_value9_2);
-
-        }
-
-        else {
-
-                printf("%s,$ %.2lf,None\n",company_name9,share_value9_1);
-
-        }
-
-
-
-	if(share_value10_1>share_value10_2) {
-
-                printf("%s,$ %.2lf,Down\n",company_name10,share_value10_2);
-
-        }
-
-        else if(share_value10_1<share_value10_2) {
-
-                printf("%s, $ %.2lf,Up\n",company_name10,share_value10_2);
-
-        }
-
-        else {
-
-                printf("%s, $ %.2lf,None\n",company_name10,share_value10_1);
-
-        }
-	
-	
-
-	return 0;
+	printf("File closed\n");
 
 }
